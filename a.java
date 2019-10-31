2f175f2868a7c2c309645f0e473520a6737ccfb7
Rupashri <rupashri.balram.7@gmail.com>
2019-10-31 19:19:28 +0530

    fixing merge errors in the program

--- a.java
+++ a.java
@@ -1,31 +1,24 @@
 
 import java.util.Scanner;
 class a{
-public static void main(String[] args){
-personalized_greeting();
-}
-String language;
+ public static void main(String[] args){
+
+ String language;
  Scanner SC=new Scanner(System.in);
+ String greetingName;
+ System.out.println("************************");
+ System.out.print("Enter a Greeting Name:) ");
+ greetingName= SC.nextLine();
  System.out.print("Enter native language - English/French/Spanish: ");
  language= SC.nextLine();
  switch(language){
- case "English": {System.out.println ("Hello Welcome to the universe of Java"); 
+ case "English": {System.out.println ("Hello" + greetingName + ", Welcome to the universe of Java"); 
 	            break;}
- case "French": {System.out.println ("Bonjour Bienvenue dans l'univers de Java");
+ case "French": {System.out.println ("Bonjour" + greetingName + ", Bienvenue dans l'univers de Java");
 	           break;}
- case "Spanish":{System.out.println ("Hola bienvenidos al universo de Java");
+ case "Spanish":{System.out.println ("Hola" + greetingName + ", bienvenidos al universo de Java");
 	            break;}
- default:{System.out.println ("Opps we dont have your lanuage configured anyway, Hello World");}	   
+ default:{System.out.println ("Opps we dont have your lanuage configured anyway, " + greetingName + ", Welcome to the universe of Java");}	   
  }
-/**Personalized Greeting Message (Roja --> 31-10-19) */
-//Input - Greeting Name
-//Output - Greeting Message 
-public static void personalized_greeting() {
-  String greetingName;
-  
-  System.out.println("************************");
-  System.out.print("Enter a Greeting Name:) ");
-  greetingName= SC.nextLine();
-  System.out.println("Welcome " + greetingName + "! to the world of java :)");
 }
 }
